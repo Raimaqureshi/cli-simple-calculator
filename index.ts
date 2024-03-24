72d93972537ad268a4ac46f40722084cb47dd715
fancyhorse <qraima1@gmail.com>
2024-03-24 13:05:55 -0700

    create simple project

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" }, 
+    { message: "Enter your second number", type: "number", name: "secondNumber" }, 
+    { message: "Please select one of the operators to perform operation", name: "operator", type: "list", choices: ["Addition", "Subtraction", "Multiplication", "Division"] }
+]);
+
+// conditional statements
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber)
+} else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber)
+} else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber)
+} else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber)
+} else {
+    console.log("Kindly select valid operator.")
+}
+
+console.log("THE END");
